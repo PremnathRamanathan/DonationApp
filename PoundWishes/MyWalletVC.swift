fabc4b257883af2c7b84dd05da280a599f174fec
himanshusharma184 <himanshu@improvonia.com>
2016-11-29 05:39:11 -0600

    created 3 swift files(MyWalletVC, CardDetailsVC, CardRegistrationVC in VC folder) and also added Navigation controller and View controller in Storyboard. Linking them is creating problem.

---
+++ PoundWishes/MyWalletVC.swift
@@ -0,0 +1,35 @@
+//
+//  MyWalletVC.swift
+//  PoundWishes
+//
+//  Created by Vishwanath Upadhayay on 10/27/16.
+//  Copyright © 2016 Group7. All rights reserved.
+//
+
+import UIKit
+
+class MyWalletVC: UIViewController {
+
+    @IBOutlet weak var AddFunds: UILabel!
+    @IBOutlet weak var AddFundsTF: UITextField!
+    @IBOutlet weak var Balance: UILabel!
+    @IBOutlet weak var BalanceTF: UITextField!
+    @IBOutlet weak var AddAmount: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
